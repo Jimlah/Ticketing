6d1f6163dcb95428449cb1a42649116877f18120
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-11-22 15:23:03 +0100

    Update Subcategory request

--- app/Http/Requests/StoreSubCategoryRequest.php
+++ app/Http/Requests/StoreSubCategoryRequest.php
@@ -13,7 +13,7 @@ class StoreSubCategoryRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,8 @@ class StoreSubCategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|unique:sub_categories,name',
+            'color' => 'required|unique:sub_categories,color',
         ];
     }
 }
