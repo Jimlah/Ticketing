764cfdf21f65049aba91526a8f11de657b027ac7
Abdullahi Temidayo Jimoh <46778885+Jimlah@users.noreply.github.com>
2021-11-23 10:35:39 +0100

    Update NewTicketCreated.php

--- app/Notifications/NewTicketCreated.php
+++ app/Notifications/NewTicketCreated.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewTicketCreated extends Notification
+class NewTicketCreated extends Notification implements ShouldQueue
 {
     use Queueable;
 
